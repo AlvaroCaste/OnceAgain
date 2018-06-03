10e90ea4190dd19877f887c7311fcefcfd4784b2
alvacast <alvacast@cisco.com>
2018-06-03 16:02:24 +0200

    Define behaviour for a failure review and rest of the successful cases

--- src/main/scala/onceagain/modules/scheduling/services/Sm2Scheduler.scala
+++ src/main/scala/onceagain/modules/scheduling/services/Sm2Scheduler.scala
@@ -5,8 +5,15 @@ import java.time.Duration
 import onceagain.modules.scheduling.model.{Response, Review}
 
 object Sm2Scheduler extends Scheduler {
-  def nextReview(review: Review, response: Response): Review = review.repetition match {
-    case 1 ⇒ review.copy(repetition = review.repetition + 1, Duration.ofDays(1))
-    case _ ⇒ review.copy(repetition = review.repetition + 1, Duration.ofDays(6))
-  }
+  def nextReview(review: Review, response: Response): Review =
+    if (response.successful)
+      review.copy(repetition = review.repetition + 1, interval = incrementedInterval(review))
+    else Review(repetition = 1, ef = review.ef)
+
+  private def incrementedInterval(review: Review) =
+    review.repetition match {
+      case 1 ⇒ Duration.ofDays(1)
+      case 2 ⇒ Duration.ofDays(6)
+      case _ ⇒ Duration.ofSeconds((review.interval.getSeconds * review.ef).toLong)
+    }
 }
--- src/test/scala/onceagain/modules/scheduling/services/Sm2SchedulerTest.scala
+++ src/test/scala/onceagain/modules/scheduling/services/Sm2SchedulerTest.scala
@@ -2,9 +2,10 @@ package onceagain.modules.scheduling.services
 
 import java.time.Duration
 
-import onceagain.modules.scheduling.model.Response.Normal
-import onceagain.modules.scheduling.model.{EasinessFactor, Review}
-import org.scalacheck.Gen
+import onceagain.modules.scheduling.model.Response.{Failed, Normal}
+import onceagain.modules.scheduling.model.{EasinessFactor, Response, Review}
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
@@ -15,10 +16,20 @@ class Sm2SchedulerTest
     with TypeCheckedTripleEquals
     with PropertyChecks {
 
+  private val repGen = Gen.posNum[Int]
+
   private val efGen =
     Gen
       .chooseNum(EasinessFactor.Min, EasinessFactor.Max, specials = EasinessFactor.Default)
       .suchThat(ef ⇒ ef >= EasinessFactor.Min && ef <= EasinessFactor.Max)
+
+  private implicit val arbReview = Arbitrary(for {
+    rep ← repGen
+    interval ← Gen.posNum[Long].map(Duration.ofDays)
+    ef ← efGen
+  } yield Review(rep, interval, ef))
+
+  private val successfulResponses = Gen.oneOf(Response.All.filter(_.successful).toSeq)
 
   "The SM2 Scheduler" should "schedule the first review in one day when response is not Failed" in {
     forAll(efGen) { ef ⇒
@@ -34,4 +45,19 @@ class Sm2SchedulerTest
     }
   }
 
+  it should "go back to the first review but keep EF after a failure" in {
+    forAll { review: Review ⇒
+      Sm2Scheduler.nextReview(review, Failed) should === (Review(repetition = 1, ef = review.ef))
+    }
+  }
+
+  it should "increment the interval factor in the general case" in {
+    forAll(arbitrary[Review], successfulResponses) { (review, response) ⇒
+      whenever(review.repetition >= 3) {
+        val nextReview = Sm2Scheduler.nextReview(review, response)
+        val proportion = nextReview.interval.getSeconds.toDouble / review.interval.getSeconds
+        proportion shouldBe review.ef +- 0.01d
+      }
+    }
+  }
 }
